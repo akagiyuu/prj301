8fa4cb3ecb23050e6390a0ba3c24b9e8fc523f3e
akagiyuu <khanghuynh0245@gmail.com>
2025-02-27 21:26:19 +0700

    feat(models/entities/User): add getters and setters

--- src/main/java/com/prj301/prj301/models/entity/User.java
+++ src/main/java/com/prj301/prj301/models/entity/User.java
@@ -27,4 +27,85 @@ public class User {
     private String bio;
 
     private LocalDate createdAt;
+
+    public long id() {
+        return id;
+    }
+
+    public User setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String password() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String avatarPath() {
+        return avatarPath;
+    }
+
+    public User setAvatarPath(String avatarPath) {
+        this.avatarPath = avatarPath;
+        return this;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String hobbies() {
+        return hobbies;
+    }
+
+    public User setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+        return this;
+    }
+
+    public LocalDate dob() {
+        return dob;
+    }
+
+    public User setDob(LocalDate dob) {
+        this.dob = dob;
+        return this;
+    }
+
+    public String bio() {
+        return bio;
+    }
+
+    public User setBio(String bio) {
+        this.bio = bio;
+        return this;
+    }
+
+    public LocalDate createdAt() {
+        return createdAt;
+    }
+
+    public User setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
 }
