fef583d24a20538ef234d9032a7a0e691fd90b36
akagiyuu <khanghuynh0245@gmail.com>
2025-03-02 12:55:19 +0700

    feat(auth/JWTUtil): generate SHA-256 key from secret

--- src/main/java/com/prj301/prj301/utils/JWTUtil.java
+++ src/main/java/com/prj301/prj301/utils/JWTUtil.java
@@ -7,31 +7,44 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 @Component
 public class JWTUtil {
     @Value("${jwt.secret-key}")
-    private static String SECRET_KEY;
+    private String SECRET_KEY;
 
-    @Value("${jwt.secret-key}")
-    private static long EXPIRATION_TIME;
+    @Value("${jwt.expiration-time}")
+    private long EXPIRATION_TIME;
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private Key key;
+
+    @PostConstruct
+    public void init() throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] keyBytes = digest.digest(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+        key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(Long id) {
-        return Jwts.builder()
-                   .setSubject(id.toString())
-                   .setIssuedAt(new Date())
-                   .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                   .signWith(key, SignatureAlgorithm.HS256)
-                   .compact();
+        return Jwts
+            .builder()
+            .setSubject(id.toString())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
+            Jwts
+                .parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJwt(token);
@@ -42,12 +55,13 @@ public class JWTUtil {
     }
 
     public long extractId(String token) throws JwtException {
-        String rawId = Jwts.parserBuilder()
-                           .setSigningKey(key)
-                           .build()
-                           .parseClaimsJwt(token)
-                           .getBody()
-                           .getSubject();
+        String rawId = Jwts
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJwt(token)
+            .getBody()
+            .getSubject();
 
         return Long.parseLong(rawId);
     }
