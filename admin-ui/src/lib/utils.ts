5109be1f1e8687f755797c05c9592458b3664808
akagiyuu <khanghuynh0245@gmail.com>
2025-03-11 18:34:17 +0700

    feat: use token stored in localStorage

--- admin-ui/src/lib/utils.ts
+++ admin-ui/src/lib/utils.ts
@@ -6,10 +6,14 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const fetchWithAuth = (url: RequestInfo | URL, init?: RequestInit) => {
+    const token = localStorage.getItem('token');
+
+    if (token === null) return null;
+
     return fetch(url, {
         ...init,
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJFUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NDE1NzY1NzIsImV4cCI6MTc0MjE4MTM3Mn0.OwmYwkpVUNYcLEfoR3r-TK0EaYL1iK8QzxY5wv8ie0RKSgMQ00uv_fISIHH-dt0_irs-bN1hDc28d2d25ffCPg',
+            Authorization: `Bearer ${token}`,
             ...init?.headers,
         },
     });
