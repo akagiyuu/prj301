b7d9c38a0f69a40156fbb7c3f50674250db8a855
akagiyuu <khanghuynh0245@gmail.com>
2025-03-13 10:27:04 +0700

    feat(admin): add chart for book and user

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -3,6 +3,9 @@ import { type RootState, type AppDispatch } from './store';
 import { Main } from './pages/main';
 import { Login } from './pages/login';
 import { Toaster } from './components/ui/sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
@@ -11,10 +14,12 @@ const App = () => {
     const token = localStorage.getItem('token');
 
     return (
-        <div>
-            {token === null ? <Login /> : <Main />}
-            <Toaster />
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div>
+                {token === null ? <Login /> : <Main />}
+                <Toaster />
+            </div>
+        </QueryClientProvider>
     );
 };
 
