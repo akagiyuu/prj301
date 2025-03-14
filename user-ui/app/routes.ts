55fbe5bf89eb4bdf98a80bf96bb1525d56a37be8
akagiyuu <khanghuynh0245@gmail.com>
2025-03-15 02:27:39 +0700

    feat(user): add navbar to layout

--- user-ui/app/routes.ts
+++ user-ui/app/routes.ts
@@ -1,12 +1,13 @@
 import {
     type RouteConfig,
     index,
+    layout,
     prefix,
     route,
 } from '@react-router/dev/routes';
 
 export default [
-    index('routes/home.tsx'),
+    layout('routes/layout.tsx', [index('routes/home.tsx')]),
     ...prefix('auth', [
         route('login', 'routes/auth/login.tsx'),
         route('signup', 'routes/auth/signup.tsx'),
