bab5bb3815779da60a78023a925b84d3c431bac8
akagiyuu <khanghuynh0245@gmail.com>
2025-03-15 20:08:13 +0700

    fix: correct api

--- user-api/src/main/java/com/prj301/user/controllers/UserController.java
+++ user-api/src/main/java/com/prj301/user/controllers/UserController.java
@@ -2,68 +2,53 @@ package com.prj301.user.controllers;
 
 import com.prj301.user.interceptors.JWTProtected;
 import com.prj301.user.models.dto.user.UserResponse;
+import com.prj301.user.models.dto.user.UserUpdate;
 import com.prj301.user.models.entity.User;
 import com.prj301.user.services.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private final UserService userService;
-    private final int startingPage = 1;
-    private final int sizeOfPage = 10;
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/find")
+    public UserResponse find(@RequestParam("username") String username) {
+        return service.findByUsername(username);
+    }
 
     @SecurityRequirement(name = "Bearer Authentication")
     @JWTProtected
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
-        return userService.toUserResponse(userService.findById(id))
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @GetMapping("/self")
+    public UserResponse self(@RequestAttribute("user-id") UUID id) {
+        return service
+            .findById(id)
+            .map(service::toUserResponse)
+            .orElse(null);
     }
 
     @SecurityRequirement(name = "Bearer Authentication")
     @JWTProtected
     @PostMapping("/update")
-    public ResponseEntity<UserResponse> updateUser(@RequestAttribute("user-id") UUID userId, @RequestBody User updatedUser) {
-        if (userId == null) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(userService.toUserResponse(userService.updateUser(userId, updatedUser)));
-    }
-    @SecurityRequirement(name = "Bearer Authentication")
-    @JWTProtected
-    @GetMapping("/")
-    public ResponseEntity<List<UserResponse>> getAllUsers(@RequestParam(defaultValue = "title") String sort_by, @RequestParam(defaultValue = "asc") String direction) {
-        Sort sort = direction.equalsIgnoreCase("asc")
-                ? Sort.by(sort_by).ascending()
-                : Sort.by(sort_by).descending();
-        Pageable pageable = PageRequest.of(startingPage, sizeOfPage, sort);
-        Page<User> userPage = userService.findAll(pageable);
-
-        if (userPage.isEmpty()) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> updateUser(@RequestAttribute("user-id") UUID id, @RequestBody UserUpdate data) {
+        if (service.update(id, data)) {
+            return ResponseEntity
+                .ok()
+                .build();
         }
 
-        List<UserResponse> userResponseList = userPage.getContent().stream()
-                .map(userService::toUserResponse)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(userResponseList);
+        return ResponseEntity
+            .badRequest()
+            .body("Failed to update");
     }
 }
--- user-api/src/main/java/com/prj301/user/services/UserService.java
+++ user-api/src/main/java/com/prj301/user/services/UserService.java
@@ -1,10 +1,9 @@
 package com.prj301.user.services;
 
 import com.prj301.user.models.dto.user.UserResponse;
+import com.prj301.user.models.dto.user.UserUpdate;
 import com.prj301.user.models.entity.User;
 import com.prj301.user.repositories.UserRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
@@ -14,51 +13,44 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
     private final UserRepository userRepository;
+
+    public UserResponse toUserResponse(User user) {
+        return UserResponse
+            .builder()
+            .username(user.getUsername())
+            .avatarPath(user.getAvatarPath())
+            .fullName(user.getFullName())
+            .hobbies(user.getHobbies())
+            .bio(user.getBio())
+            .build();
+    }
 
     public Optional<User> findById(UUID id) {
         return userRepository.findById(id);
     }
 
-    public Page<User> findAll(Pageable pageable) {
-        return userRepository.findAll(pageable);
+    public UserResponse findByUsername(String username) {
+        return userRepository
+            .findByUsernameContainsIgnoreCase(username)
+            .map(this::toUserResponse)
+            .orElse(null);
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
+    public boolean update(UUID id, UserUpdate data) {
+        try {
+            userRepository.updateAvatarPathAndFullNameAndHobbiesAndDobAndBioById(
+                data.getAvatarPath(),
+                data.getFullName(),
+                data.getHobbies(),
+                data.getDob(),
+                data.getBio(),
+                id
+            );
 
-    public User save(User user) {
-        return userRepository.save(user);
-    }
-
-    public User updateUser(UUID id, User updatedUser) {
-        return userRepository.findById(id).map(user -> {
-            user.setUsername(updatedUser.getUsername());
-            user.setFullName(updatedUser.getFullName());
-            user.setHobbies(updatedUser.getHobbies());
-            user.setDob(updatedUser.getDob());
-            user.setBio(updatedUser.getBio());
-            return userRepository.save(user);
-        }).orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    // Mapping method from User to UserResponse DTO
-    public UserResponse toUserResponse(User user) {
-        return UserResponse.builder()
-                .username(user.getUsername())
-                .avatarPath(user.getAvatarPath())
-                .fullName(user.getFullName())
-                .hobbies(user.getHobbies())
-                .bio(user.getBio())
-                .build();
-    }
-
-    public Optional<UserResponse> toUserResponse(Optional<User> userOptional){
-        if(userOptional.isPresent()){
-            return Optional.of(toUserResponse(userOptional.get()));
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return Optional.empty();
     }
 }
