595274c94d2d0c973bc80a0abf393ced8345c1a1
akagiyuu <khanghuynh0245@gmail.com>
2025-03-20 21:51:38 +0700

    fix: use multipart form for update api

--- user-api/src/main/java/com/prj301/user/repositories/UserRepository.java
+++ user-api/src/main/java/com/prj301/user/repositories/UserRepository.java
@@ -13,16 +13,4 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameContainsIgnoreCase(String username);
-
-    @Transactional
-    @Modifying
-    @Query("update User u set u.avatarPath = ?1, u.fullName = ?2, u.hobbies = ?3, u.dob = ?4, u.bio = ?5 where u.id = ?6")
-    void updateAvatarPathAndFullNameAndHobbiesAndDobAndBioById(
-        @Nullable String avatarPath,
-        @Nullable String fullName,
-        @Nullable String hobbies,
-        @Nullable LocalDate dob,
-        @Nullable String bio,
-        UUID id
-    );
 }
